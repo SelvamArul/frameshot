46ff005a072b9deca63dfb96dea2d775208ecbea
periyasa <finditarul@gmail.com>
2017-02-24 11:35:41 +0100

    frameshot: launch file

---
+++ launch.sh
@@ -0,0 +1,4 @@
+rosrun frameshot frameshot _mode:=mbzirc _folder_name:=/tmp _stereo_left_topic:=/vertical_stereo/left/image_raw _stereo_right_topic:=/vertical_stereo/right/image_raw _stereo_left_cam_info:=/stereo/left/camera_info _stereo_right_cam_info:=/stereo/right/camera_info _tof_topic:=/pico_flexx/points _tof_cam_info:=/pico_flexx/camera_info
+
+
+#/home/arul/mbzirc/wrench_perception/data/sync_data
